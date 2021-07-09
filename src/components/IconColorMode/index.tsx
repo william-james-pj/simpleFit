5323141275c8396d2b476a758a01dccbb0c17820
William James <william.james.pj@gmail.com>
2021-07-08 21:52:39 -0300

    StatusBar change color

--- src/components/IconColorMode/index.tsx
+++ src/components/IconColorMode/index.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { StatusBar } from "expo-status-bar";
+
 import { useDarkMode } from "../../hooks/useDarkMode";
 
 import * as S from "./styles";
@@ -13,6 +15,7 @@ export function IconColorMode() {
         size={25}
         color={"red"}
       />
+      <StatusBar style={theme.title === "light" ? "dark" : "light"} />
     </S.ModeButtonView>
   );
 }
